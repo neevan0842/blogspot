1619550c284b8a62a9bc995c1562ec4f06c3db6b
neevan0842 <manojnaveen085@gmail.com>
2024-09-29 11:04:26 +0530

    In Linux

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,12 +1,6 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-// const apiUrl = "http://139.59.68.123";
-
-// const api = axios.create({
-//   baseURL: apiUrl,
-// });
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
